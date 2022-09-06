2d0320e1d963d89a9aef10e8f19cdc88bba6b096
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-09-06 12:21:03 -0600

    add create new categories feature on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
     @products = Product.count(:name)
-    @categories = Category.count(:name)
+    @categories = Category.count
   end
+
 end
