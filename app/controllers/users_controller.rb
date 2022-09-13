26759be3f597d2310596e39f74aed1dd65473316
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-09-13 11:13:12 -0600

    add html view for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,29 +6,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    
-    # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
-    @user.email.downcase!
-    
-    if @user.save
-      # If user saves in the db successfully:
-      flash[:notice] = "Account created successfully!"
-      redirect_to root_path
+   
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
     else
-      # If user fails model validation - probably a bad password or duplicate email:
-      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
-      render :new
+      redirect_to '/signup'
     end
   end
 
-private
+  private
 
   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
     # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
     # that can be submitted by a form to the user model #=> require(:user)
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
-# ----- end of added lines -----
+
 end
+
